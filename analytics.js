848d11a2781e0355567a599a0a7aba49d64c192a
markpy1 <58368825+markpy1@users.noreply.github.com>
2026-04-07 15:42:46 +0100

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,5 @@
+// Gatley Kitchens - Google Analytics 4
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-013W04141C');
